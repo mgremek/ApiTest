9a1889bb82836acd3404a29fb845a2c8d59d95a5
mgremek <k.pawlak666@gmail.com>
2023-09-04 19:57:08 +0200

    graphQl added

---
+++ Api_Cats/Api/Query.cs
@@ -0,0 +1,14 @@
+﻿using Api_Cats.Entities;
+using Api_Cats.Services;
+using System.Numerics;
+
+namespace Api_Cats.Api
+{
+    public class Query
+    {
+        public async Task<IEnumerable<Cat>> GetCatsAsync([Service] ICatsQlService catsService)
+        {
+            return await catsService.GetCatsAsync();
+        }
+    }
+}
